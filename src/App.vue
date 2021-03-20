e43d081d97d1bb8c031b5032a710f4482488b683
Michael Raichelson <i.am@michaelraichelson.com>
2021-03-20 18:10:53 -0500

    Getting things rolling and getting Prettier configured.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="app">{{ txt }}</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  data() {
+    return {
+      txt: 'hello from vue',
+    }
   },
-};
+}
 </script>
 
 <style lang="scss">
