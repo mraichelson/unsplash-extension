d924fa127fd97877e2f406eb7d3d9ac38161ff7f
Michael Raichelson <i.am@michaelraichelson.com>
2021-03-20 22:54:54 -0500

    A start on getting fields into place and basic Vue stuff set up.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
 <template>
-  <div id="app">{{ txt }}</div>
+  <div id="app">
+    <form action="#">
+      <label for="width">Width</label>
+      <input
+        type="number"
+        id="width"
+        name="width"
+        v-model="width"
+        min="1"
+        required
+      />
+      <br />
+      <label for="height">Height</label>
+      <input
+        type="number"
+        id="height"
+        name="height"
+        v-model="height"
+        min="1"
+        required
+      />
+      <br />
+      <label for="greyscale">
+        <input
+          type="checkbox"
+          id="greyscale"
+          name="greyscale"
+          v-model="greyscale"
+        />
+        Greyscale
+      </label>
+      <details>
+        <summary>Advanced options</summary>
+        <div>Lorem ipsum dolor sit amet.</div>
+      </details>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -7,19 +43,35 @@ export default {
   name: 'App',
   data() {
     return {
-      txt: 'hello from vue',
+      width: 320,
+      height: 240,
+      greyscale: false
     }
-  },
+  }
 }
 </script>
 
 <style lang="scss">
-#app {
+$light: #eef;
+$dark: #113;
+
+html,
+body {
+  background-color: $light;
+  color: $dark;
+  padding: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+// #app {}
+label {
+  display: block;
+  margin: 1rem 0 0.25rem;
+}
+details {
+  summary {
+    cursor: pointer;
+  }
 }
 </style>
